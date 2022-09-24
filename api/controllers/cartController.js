0e6b53bb0469f600b92a886b7edeb7f69c578f1c
Diego Perez <95981958+diegoQuinas@users.noreply.github.com>
2022-09-24 10:01:32 -0300

    ADD: Añadida funcion modifyCart

--- api/controllers/cartController.js
+++ api/controllers/cartController.js
@@ -1,12 +1,23 @@
-const persistance = require('../persistence/persistence')
+const persistance = require("../persistence/persistence");
 
-const listCart = (req, res) => {
-  const cart = persistance.getCartByUserID(req.params.id);
-  if (cart) {
-    res.send(cart)
+const cartController = {
+  listCart: (req, res) => {
+    const cart = persistance.getCartByUserID(req.params.id);
+    if (cart) {
+      res.send(cart);
+    }
+    res.json({
+      ok: false,
+      msg: "No se encontraron usuarios con el id " + res.params.id,
+    });
+  },
+  modifyCart: (req, res) => {
+    persistance.deleteCartByUserId(req.params.id);
+    const newCart = req.body
+    newCart.forEach(product=>persistance.inster(Cart, {id_product: product.id, id_user: req.params.id}))
+    const modifiedCart = persistance.getCartByUserID(req.params.id);
+    res.json({msg: 'Carrito modificado', user: req.params.id, cart: modifiedCart })
   }
-  res.json({ok: false, msg: "No se encontraron usuarios con el id " + res.params.id})
-}
+};
 
-
-module.exports = { getCarrito, putCarrito };
+module.exports = cartController;
