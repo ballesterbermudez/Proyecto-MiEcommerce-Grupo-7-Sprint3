95511efaf057f7cd4d93f9c61f2341bfa2d46e5d
Diego Perez <95981958+diegoQuinas@users.noreply.github.com>
2022-09-26 16:17:11 -0300

    ADD: Validacion de stock en carts

--- api/middelware/validateProducts.js
+++ api/middelware/validateProducts.js
@@ -1,29 +1,29 @@
 const persistence = require("../persistence/persistence");
 
+//Funcion auxiliar
+//PRE: Recibe un id de un producto y la cantidad a agregar al carrito
+//POS: Consulta a la base de datos el stock y verifica que la cantidad no sea mayor al stock
+const checkStock = (id_product, quantity) => {
+  const product = persistence.searchById("Product", id_product);
+  if (product.stock < quantity) return false;
+  return true;
+};
+
 module.exports = async (req, res, next) => {
   const cart = req.body;
   let result = null;
   try {
-    // cart.forEach(async (product) => {
-    //   const theProduct = await persistence.searchById(
-    //     "Product",
-    //     product.id_product
-    //   );
-    //   console.log(theProduct);
-    //   if (theProduct == null) {
-    //     result = {
-    //       ok: false,
-    //       msg: `El producto con id ${product.id_product} no existe en la base de datos`,
-    //     };
-    //   }
-    // });
-
+    if (!checkStock(cart[i].id_product, cart[i].quantity)) {
+      result = {
+        ok: false,
+        msg: `No hay suficiente stock para agregar el producto con id ${cart[i].id_product} al carrito`,
+      };
+    }
     for (let i = 0; i < cart.length; i++) {
       const theProduct = await persistence.searchById(
         "Product",
         cart[i].id_product
       );
-      console.log(theProduct);
       if (theProduct == null) {
         result = {
           ok: false,
