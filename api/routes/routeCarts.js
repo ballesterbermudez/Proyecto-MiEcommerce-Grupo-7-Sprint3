3a7fb0359daf3c501ebd162fa2cf05f08c789cc0
ballesterbermudez <ballester.bermudez@gmail.com>
2022-10-03 16:37:26 -0300

    Estructuracion para la realizacion de test

--- api/routes/routeCarts.js
+++ api/routes/routeCarts.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 const cartController = require("../controllers/cartController");
 const validateUserId = require("../middelware/validateUserId");
-const validateProducts = require("../middelware/validateProducts");
 
 router.get("/:id", validateUserId, cartController.listCart);
 router.put("/:id", validateUserId, cartController.modifyCart);
