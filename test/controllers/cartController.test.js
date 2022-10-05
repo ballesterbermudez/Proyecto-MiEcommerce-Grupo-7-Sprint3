ff494a7cceb55eba8533289baf7312d50975bdf0
Diego Perez <95981958+diegoQuinas@users.noreply.github.com>
2022-10-04 22:01:00 -0300

    ADD: Agregado test de status del get

---
+++ test/controllers/cartController.test.js
@@ -0,0 +1,44 @@
+const request = require("supertest");
+const db = require("../../api/database/models");
+const { app, server } = require("../../server");
+const gerateJWT = require("../../helpers/generateToken");
+const cartController = require("../../api/controllers/cartController");
+
+afterEach(() => {
+  server.close();
+});
+
+afterAll(async () => {
+  await db.sequelize.close();
+});
+
+describe("GET /api/v1/carts/", () => {
+  test("debe devolver status 200", async () => {
+    const idUser = 1;
+
+    const payload = {
+      id: 1,
+      username: "diegogod",
+      role: "GOD",
+    };
+    const jwt = await gerateJWT(payload);
+    const { statusCode } = await request(app)
+      .get("/api/v1/carts/" + idUser)
+      .auth(jwt, { type: "bearer" });
+    expect(statusCode).toBe(200);
+  });
+
+  test("debe devolver un objeto json", async () => {
+    let id = 3;
+    const payload = {
+      id: 1,
+      username: "diegogod",
+      role: "GOD",
+    };
+    const jwt = await gerateJWT(payload);
+    const { body } = await request(app)
+      .get(`/api/v1/pictures/${id}`)
+      .auth(jwt, { type: "bearer" });
+    expect(body).toBeInstanceOf(Object);
+  });
+});
