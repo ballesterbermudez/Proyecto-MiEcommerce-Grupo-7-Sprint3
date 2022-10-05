c1430d2efa6e327f1b1a8801119c5ead7e9cb839
federicojsd <fedesierra@gmail.com>
2022-10-05 11:01:10 -0300

    Se agrega test de validate email unique

--- test/controllers/usersController.test.js
+++ test/controllers/usersController.test.js
@@ -11,6 +11,7 @@ afterAll(async () => {
   await db.sequelize.close();
 });
 
+//USADO PARA CREAR TOKEN
 const payload = {
   id: 1,
   username: "diegogod",
@@ -88,27 +89,168 @@ describe("GET /users/:id", () => {
     expect(body.user).toEqual(
       expect.objectContaining({ email: expect.any(String) })
     );
-  });  
+  });
 });
 
 //CREAR USUARIO
 describe("POST /users", () => {
-    test("Debe retornar un statusCode 200", async () => {
-      const newUser = {
-        email: "supertest@cenco.com",
-        username: "jest",
-        password: "123456",
-        first_name: "El",
-        last_name: "Tester",
-        profilepic: "https://sequelize.com/constraints/",
-        id_role: 2,
-      };
-      const token = await generateJWT(payload);
-      const {statusCode} = await request(app)
-        .post(`/api/v1/users/`)
-        .auth(token, { type: "bearer" })
-        .send(newUser);
-      expect(statusCode).toBe(200);
-    });
-    
+  test.skip("Debe retornar un statusCode 200", async () => {
+    const newUser = {
+      email: "supertest@cenco.com",
+      username: "jest",
+      password: "123456",
+      first_name: "El",
+      last_name: "Tester",
+      profilepic: "https://sequelize.com/constraints/",
+      id_role: 1,
+    };
+    const token = await generateJWT(payload);
+    const { statusCode } = await request(app)
+      .post(`/api/v1/users/`)
+      .auth(token, { type: "bearer" })
+      .send(newUser);
+    expect(statusCode).toBe(200);
   });
+
+  test("Debe retornar un statusCode 412 por error en ROL", async () => {
+    const newUser = {
+      email: "supertest@cenco.com",
+      username: "jest",
+      password: "123456",
+      first_name: "El",
+      last_name: "Tester",
+      profilepic: "https://sequelize.com/constraints/",
+      id_role: 999,
+    };
+    const token = await generateJWT(payload);
+    const { statusCode } = await request(app)
+      .post(`/api/v1/users/`)
+      .auth(token, { type: "bearer" })
+      .send(newUser);
+    expect(statusCode).toBe(412);
+  });
+
+  test("Debe retornar un statusCode 412 por error en atributos", async () => {
+    const newUser = {
+      email: "supertest@cenco.com",
+      username: "jest",
+      password: "123456",
+      profilepic: "https://sequelize.com/constraints/",
+      id_role: 2,
+    };
+    const token = await generateJWT(payload);
+    const { statusCode } = await request(app)
+      .post(`/api/v1/users/`)
+      .auth(token, { type: "bearer" })
+      .send(newUser);
+    expect(statusCode).toBe(412);
+  });
+
+  test("Debe retornar un statusCode 401 validate email unique", async () => {
+    const newUser = {
+      email: "diego@god.com",
+      username: 123,
+      password: "123456",
+      first_name: "El",
+      last_name: "Tester",
+      profilepic: "https://sequelize.com/constraints/",
+      id_role: 1,
+    };
+    const token = await generateJWT(payload);
+    const { statusCode } = await request(app)
+      .post(`/api/v1/users/`)
+      .auth(token, { type: "bearer" })
+      .send(newUser);
+    expect(statusCode).toBe(401);
+  });
+
+  test("Debe retornar un statusCode 401 validate username no es String", async () => {
+    const newUser = {
+      email: "supertest@cenco.com",
+      username: 123,
+      password: "123456",
+      first_name: "El",
+      last_name: "Tester",
+      profilepic: "https://sequelize.com/constraints/",
+      id_role: 1,
+    };
+    const token = await generateJWT(payload);
+    const { statusCode } = await request(app)
+      .post(`/api/v1/users/`)
+      .auth(token, { type: "bearer" })
+      .send(newUser);
+    expect(statusCode).toBe(401);
+  });
+
+  test("Debe retornar un statusCode 401 validate username sin letra", async () => {
+    const newUser = {
+      email: "supertest@cenco.com",
+      username: "123",
+      password: "123456",
+      first_name: "El",
+      last_name: "Tester",
+      profilepic: "https://sequelize.com/constraints/",
+      id_role: 1,
+    };
+    const token = await generateJWT(payload);
+    const { statusCode } = await request(app)
+      .post(`/api/v1/users/`)
+      .auth(token, { type: "bearer" })
+      .send(newUser);
+    expect(statusCode).toBe(401);
+  });
+
+  test("Debe retornar un statusCode 401 validate password no es String", async () => {
+    const newUser = {
+      email: "supertest@cenco.com",
+      username: "123",
+      password: 123456,
+      first_name: "El",
+      last_name: "Tester",
+      profilepic: "https://sequelize.com/constraints/",
+      id_role: 1,
+    };
+    const token = await generateJWT(payload);
+    const { statusCode } = await request(app)
+      .post(`/api/v1/users/`)
+      .auth(token, { type: "bearer" })
+      .send(newUser);
+    expect(statusCode).toBe(401);
+  });
+
+  test("Debe retornar un statusCode 401 validate password menor a 6", async () => {
+    const newUser = {
+      email: "supertest@cenco.com",
+      username: "123",
+      password: "123",
+      first_name: "El",
+      last_name: "Tester",
+      profilepic: "https://sequelize.com/constraints/",
+      id_role: 1,
+    };
+    const token = await generateJWT(payload);
+    const { statusCode } = await request(app)
+      .post(`/api/v1/users/`)
+      .auth(token, { type: "bearer" })
+      .send(newUser);
+    expect(statusCode).toBe(401);
+  });
+
+  test("Debe retornar un statusCode 401 validate email incorrecto", async () => {
+    const newUser = {
+      email: "supertest@cenco.com",
+      username: "123",
+      password: "123",
+      first_name: "El",
+      last_name: "Tester",
+      profilepic: "https://sequelize.com/constraints/",
+      id_role: 1,
+    };
+    const token = await generateJWT(payload);
+    const { statusCode } = await request(app)
+      .post(`/api/v1/users/`)
+      .auth(token, { type: "bearer" })
+      .send(newUser);
+    expect(statusCode).toBe(401);
+  });
+});
