bf33d0d4763bb47f5d882fcdff89f8f6873a7d4c
federicojsd <fedesierra@gmail.com>
2022-10-04 21:04:07 -0300

    Se agrega test de listar usuario por id

--- test/controllers/usersController.test.js
+++ test/controllers/usersController.test.js
@@ -10,24 +10,22 @@ afterAll(async () => {
     await db.sequelize.close();
 })
 
+const payload = {
+    id: 1,
+    username: 'diegogod',
+    role: 'GOD',
+  };
+ 
+
+
 describe('GET /usuarios', ()=>{
     test('Debe retornar un statusCode 200', async ()=>{
-        const payload = {
-            id: 1,
-            username: 'diegogod',
-            role: 'GOD',
-          };
         const token = await generateJWT(payload);
         const { statusCode } = await request(app).get('/api/v1/users').auth(token, {type: 'bearer'});
         expect(statusCode).toBe(200);
     })
 
-    test('Debe retornar un statusCode 500', async ()=>{
-        const payload = {
-            id: 1,
-            username: 'diegogod',
-            role: 'GOD',
-          };
+    test.skip('Debe retornar un statusCode 500', async ()=>{
         const token = await generateJWT(payload);
         await db.sequelize.close();
         const { statusCode } = await request(app).get('/api/v1/users').auth(token, {type: 'bearer'});
@@ -36,14 +34,25 @@ describe('GET /usuarios', ()=>{
 })
 
 describe('GET /usuario/:id', () => {
-    // test('Debe retornar un statusCode 200', async ()=>{
-    //     const payload = {
-    //         id: 1,
-    //         username: 'diegogod',
-    //         role: 'GOD',
-    //       };
-    //     const token = await generateJWT(payload);
-    //     const { statusCode } = await request(app).get('/api/v1/users').auth(token, {type: 'bearer'});
-    //     expect(statusCode).toBe(200);
-    // })
+    test('Debe retornar un statusCode 200', async ()=>{
+        const userId = "1";
+        const token = await generateJWT(payload);
+        const { statusCode } = await request(app).get(`/api/v1/users/${userId}`).auth(token, {type: 'bearer'});
+        expect(statusCode).toBe(200);
+    })
+
+    test('Debe retornar un statusCode 401', async ()=>{
+        const userId = "999";
+        const token = await generateJWT(payload);
+        const { statusCode } = await request(app).get(`/api/v1/users/${userId}`).auth(token, {type: 'bearer'});
+        expect(statusCode).toBe(401);
+    })
+
+    test.skip('Debe retornar un statusCode 500', async ()=>{
+        const userId = "1";
+        const token = await generateJWT(payload);
+        await db.sequelize.close();
+        const { statusCode } = await request(app).get(`/api/v1/users/${userId}`).auth(token, {type: 'bearer'});
+        expect(statusCode).toBe(500);
+    })
 })
